27c0299c3fac64e5b6a1dd630046e2a67b85409c
cdupont9 <cdupontcolumbia@gmail.com>
2024-08-13 18:30:37 -0400

    changes for unit 18

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 window.addEventListener('scroll', function() {
     const imgContainer = document.querySelector('.img-container');
-    // For fixed positioning, CSS should handle this; JavaScript here is for additional logic if needed
-    // Example: Change opacity or add other styles based on scroll position
+    
 });
